720c3eef9f7c2eaf86e1b206a54110160266923d
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2026-03-18 06:19:26 -0300

    Create --> Accounts App
    - Create --> UserModel + Manager + QuerySet.

--- core/settings.py
+++ core/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_components',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -170,6 +171,8 @@ STATICFILES_FINDERS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.UserModel'
 
 # Django Components
 COMPONENTS = ComponentsSettings(
