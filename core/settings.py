804d2aa911c0290af7ceb5fe96b08b774eb33af1
BlueLuscious <117128204+BlueLuscious@users.noreply.github.com>
2026-03-24 17:03:05 -0300

    Create --> Models
    Models:
    - Brand
    - Category
    - Product
    - Product Variant
    - Product Image
    - Cart
    - Cart Item
    - Quote
    - Quote Item
    
    Also:
    - Create --> Manager + QuerySet + DTO + Factory for each Model.
    - Add --> TestCases.

--- core/settings.py
+++ core/settings.py
@@ -49,6 +49,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_components',
     'accounts',
+    'masterdata',
+    'catalog',
+    'cart',
+    'quotation',
 ]
 INSTALLED_APPS += list(dict.fromkeys(MEDIA_STORAGE_CONFIG.extra_apps + STATIC_STORAGE_CONFIG.extra_apps))
 
